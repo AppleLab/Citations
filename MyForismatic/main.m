362cda5364baae149dc059b451736f38b53749be
Airat <mulukov.air@gmail.com>
2014-08-21 14:10:53 +0400

    Initial Commit

---
+++ MyForismatic/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyForismatic
+//
+//  Created by User on 21.08.14.
+//  Copyright (c) 2014 AiratMulyukovIncorporated. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
